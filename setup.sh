fa18f82b93112951b59e95aaf33a4ddb5809b939
MacroUniverse <270174408@qq.com>
2022-11-18 23:30:20 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,18 +9,22 @@ if [[ ! -f "$FILE" ]]; then
 fi
 done
 
-source ../SLISC0/make/lib_append.sh $PWD/arb-*
-source ../SLISC0/make/lib_append.sh $PWD/arpack-ng-*
-source ../SLISC0/make/lib_append.sh $PWD/flint2-*
-source ../SLISC0/make/lib_append.sh $PWD/gmp-*
-source ../SLISC0/make/lib_append.sh $PWD/gsl-*
-source ../SLISC0/make/lib_append.sh $PWD/lapack64-shared-*
-source ../SLISC0/make/lib_append.sh $PWD/lapack64-static-*
-source ../SLISC0/make/lib_append.sh $PWD/lapack-shared-*
-source ../SLISC0/make/lib_append.sh $PWD/lapack-static-*
-source ../SLISC0/make/lib_append.sh $PWD/mkl-*
-source ../SLISC0/make/lib_append.sh $PWD/mpfr-*
-source ../SLISC0/make/lib_append.sh $PWD/sqlite-*
-source ../SLISC0/make/lib_append.sh $PWD/boost-1.80.0
-# source ../SLISC0/make/lib_append.sh $PWD/boost-filesystem-*
-# source ../SLISC0/make/lib_append.sh $PWD/boost-1.76.0*
+ADD="source ../SLISC0/make/lib_append.sh $PWD"
+
+$ADD/arb-2.23.0
+$ADD/arpack-ng-3.8.0
+# $ADD/boost-1.76.0-GCC-10.3.0
+$ADD/boost-1.80.0
+$ADD/flint2-2.9.0
+$ADD/gmp-6.1.2
+$ADD/gsl-2.7.1
+$ADD/lapack64-shared-3.10.1
+$ADD/lapack64-static-3.10.1
+$ADD/lapack-shared-3.10.1
+$ADD/lapack-static-3.10.1
+$ADD/mkl-2021a
+$ADD/mpfr-4.1.0
+$ADD/mplapack-2.0.1
+$ADD/sqlite-3.40.0
+# $ADD/boost-filesystem-*
+# $ADD/boost-1.76.0*
