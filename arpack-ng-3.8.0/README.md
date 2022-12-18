0c08e24a10d559500b42e2c6aeb8cc700a2b4778
MacroUniverse <270174408@qq.com>
2022-12-18 00:19:37 -0600

    update arpack (4 versions)

--- arpack-ng-3.8.0/README.md
+++
@@ -1,1 +0,0 @@
-compiled from source in docker addiszx/slisc0:centos7.9.2009-portable
--- setup.sh
+++ setup.sh
@@ -14,9 +14,11 @@ done
 ADD="source ../SLISC0/make/lib_prepend.sh $PWD"
 
 $ADD/arb-2.23.0
-$ADD/arpack-ng-3.8.0
+$ADD/arpack-ng64-shared-3.8.0
+$ADD/arpack-ng64-static-3.8.0
+$ADD/arpack-ng-shared-3.8.0
+$ADD/arpack-ng-static-3.8.0
 $ADD/arpackpp-2.3.0
-# $ADD/boost-1.76.0-GCC-10.3.0
 $ADD/boost-1.80.0
 $ADD/eigen-3.4.0
 $ADD/flint2-2.9.0
@@ -30,5 +32,6 @@ $ADD/mkl-2021a
 $ADD/mpfr-4.1.0
 $ADD/mplapack-2.0.1
 $ADD/sqlite-3.40.0
+# $ADD/boost-1.76.0-GCC-10.3.0
 # $ADD/boost-filesystem-*
 # $ADD/boost-1.76.0*
